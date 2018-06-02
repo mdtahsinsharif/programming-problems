fe9ab1e0e760cba4f51535f5426dd3fa6f47ae09
Md Tahsin Sharif <mt2shari@uwaterloo.ca>
2018-06-02 15:06:05 -0400

    First recurring character

---
+++ FirstRecurringCharacter.java
@@ -0,0 +1,25 @@
+﻿import java.util.HashMap;
+
+/* Find first recurring character in a string. */
+
+public class FirstRecurringCharacter {
+    
+    public static char first_recurring_char(String st){
+        char[] ch = st.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i = 0; i<ch.length; i++){
+            if(map.containsKey(ch[i])){
+                return ch[i];
+            } else {
+                map.put(ch[i], 1);
+            }
+        }
+        return '0';
+    }
+    
+    public static void main(String args[]) {
+        
+
+        System.out.println("Recurring: " + first_recurring_char("tahsht"));
+    }
+}
