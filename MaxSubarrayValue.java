4e99c0a34b4c96155c5a771bcd678ca0e073cb7a
Md Tahsin Sharif <mt2shari@uwaterloo.ca>
2018-06-02 15:26:31 -0400

    Max Subarray Value

---
+++ MaxSubarrayValue.java
@@ -0,0 +1,19 @@
+﻿public class MaxSubarray {
+    public static int max_subarray_sum(int[] arr){
+        int max_upto_here = 0, max_so_far = 0;
+        for(int i=0; i<arr.length; i++){
+            max_upto_here = max_upto_here + arr[i];
+            if(max_upto_here < 0)
+                max_upto_here = 0;
+                
+            if(max_so_far < max_upto_here)
+                max_so_far = max_upto_here;
+        }
+        return max_so_far;
+    }
+    
+    public static void main(String args[]) {
+        int[] arr = new int[]{-2, -3, 4, -1, -2, 1, 5, -3, -2, -1, 8, 9, 8, -1};
+        System.out.println("Max subarray sum = " + max_subarray_sum(arr));
+    }
+}
